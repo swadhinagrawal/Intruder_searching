fb5abe05217ca9720a99684842c956f4d4807de5
zorawar12 <swadhin12a@gmail.com>
2022-09-02 20:02:11 +0530

    Probabilistic method done

--- randomized_static_intruder_searching.py
+++ randomized_static_intruder_searching.py
@@ -7,6 +7,24 @@ class Node:
         self.node_id = np.array([int(id/arena_w),id%arena_w])
         self.probability = 1/(arena_h*arena_w)
         self.cost = 0
+        self.distance = None
+        self.parent = None
+        self.f = 0
+        self.neighbors = []
+    
+    def add_neighbors(self,grid,column,row):
+
+        neighbor_x = self.node_id[0]
+        neighbor_y = self.node_id[1]
+    
+        if neighbor_x < column - 1:
+            self.neighbors.append(grid[neighbor_x+1][neighbor_y])
+        if neighbor_x > 0:
+            self.neighbors.append(grid[neighbor_x-1][neighbor_y])
+        if neighbor_y < row:
+            self.neighbors.append(grid[neighbor_x][neighbor_y +1])
+        if neighbor_y > 0: 
+            self.neighbors.append(grid[neighbor_x][neighbor_y-1])
 
 class Robot:
     def __init__(self,id,arena_w,arena_h,nodes):
@@ -83,6 +101,147 @@ def path_finder(origin,goal,grid,fig,ax):
 
     return path
 
+
+def to_be_visited(num_grids,visited_and_distance):
+    v = -10
+    for i in range(num_grids):
+        if visited_and_distance[i][0] == 0 and (v < 0 or visited_and_distance[i][1] <= visited_and_distance[v][1]):
+            v = i
+    return v
+
+def cost_map_(grids):
+    cost = np.zeros_like(grids)
+    for i in range(grids.shape[0]):
+        for j in range(grids.shape[1]):
+            cost[i,j] = grids[i,j].cost
+    return cost
+
+def Astar(grids,source,destination):
+    # plt.ion()
+    # fig1,ax1 = plt.subplots()
+    inner_mat = np.zeros((grids.shape[1],grids.shape[1]))
+    outter_mat = np.zeros((grids.shape[0]*grids.shape[1],grids.shape[0]*grids.shape[1]))
+
+    for i in range(grids.shape[1]):
+        if i+1<grids.shape[1]:
+            inner_mat[i,i+1] = 1
+        if i-1>=0:
+            inner_mat[i,i-1] = 1
+    
+    for i in range(outter_mat.shape[1]):
+        if i+grids.shape[1]<outter_mat.shape[1]:
+            outter_mat[i,i+grids.shape[1]] = 1
+        if i-grids.shape[1]>=0:
+            outter_mat[i,i-grids.shape[1]] = 1
+    
+    vertices = np.kron(np.eye(grids.shape[0]),inner_mat) + outter_mat
+    edges = np.copy(vertices)
+    temp_grids = np.copy(grids)
+    cost_map = cost_map_(grids)
+    # plot_mesh(fig1,ax1,grids.shape[1],grids.shape[0],cost_map)
+    num_grids = len(vertices[0])
+    temp_grids[source[0],source[1]].f = 0
+    temp_grids[source[0],source[1]].distance = 0
+    temp_grids[destination[0],destination[1]].f = 0
+    open_nodes = [temp_grids[source[0],source[1]]]
+    closed_nodes = []
+
+    while len(open_nodes) > 0:
+        current_node = open_nodes[0]
+        
+        # ax1.scatter([current_node.node_id[1]+0.5],[current_node.node_id[0]+0.5],color = 'green')
+        current_index = 0
+        for i, item in enumerate(open_nodes):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = i
+        # ax1.scatter([current_node.node_id[1]+0.5],[current_node.node_id[0]+0.5],color = 'orange')
+        
+        # Pop current off open list, add to closed list
+        open_nodes.pop(current_index)
+        closed_nodes.append(current_node)
+
+        # Found the goal
+        if current_node == temp_grids[destination[0],destination[1]]:
+            # ax1.scatter([destination[1]+0.5],[destination[0]+0.5],color = 'violet')
+            # ax1.scatter([source[1]+0.5],[source[0]+0.5],color = 'black')
+
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.node_id)
+                # ax1.scatter([current.node_id[1]+0.5],[current.node_id[0]+0.5],color='brown',s=50)
+                current = current.parent
+                
+                
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.node_id[0] + new_position[0], current_node.node_id[1] + new_position[1])
+            
+        
+            # Make sure within range
+            if (node_position[0] < 0 or node_position[0] > grids.shape[0]-1) or (node_position[1] > grids.shape[1]-1 or node_position[1] < 0):
+                continue
+
+            # Create new node
+            new_node = temp_grids[node_position[0],node_position[1]]
+
+            # Append
+            if np.linalg.norm(np.array(node_position)-source)!=0:
+                # ax1.scatter([node_position[1]+0.5],[node_position[0]+0.5],color = 'brown')
+                children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            # ax1.scatter([child.node_id[1]+0.5],[child.node_id[0]+0.5],edgecolors = 'white',facecolor='none')
+            # Child is on the closed list
+            for closed_child in closed_nodes:
+                if child == closed_child:
+                    # ax1.scatter([child.node_id[1]+0.5],[child.node_id[0]+0.5],color = 'orange')
+                    continue
+            
+            if child not in closed_nodes:
+                # Create the f, g, and h values
+                if child==temp_grids[source[0],source[1]]:
+                    print('Doomed')
+                child.distance = current_node.distance + 1
+                child.f = child.distance + child.cost
+                child.parent = current_node
+                # ax1.plot([child.node_id[1]+0.5,child.parent.node_id[1]+0.5],[child.node_id[0]+0.5,child.parent.node_id[0]+0.5],color= 'black')
+                # ax1.scatter([child.parent.node_id[1]+0.5],[child.parent.node_id[0]+0.5],edgecolors= 'red',facecolor='none')
+
+            # Child is already in the open list
+            for open_node in open_nodes:
+                if child == open_node and child.distance > open_node.distance and child.cost > open_node.cost:
+                    # ax1.scatter([child.node_id[1]+0.5],[child.node_id[0]+0.5],color = 'cyan')
+                    continue
+
+            # Add the child to the open list
+            if child not in open_nodes+closed_nodes:
+                open_nodes.append(child)
+            # ax1.scatter([destination[1]+0.5],[destination[0]+0.5],color = 'violet')
+            # ax1.scatter([source[1]+0.5],[source[0]+0.5],color = 'black')
+
+    print(path)
+    # visited_and_distance = [[0,np.Infinity,cost_map[int(i/grids.shape[1]),i%grids.shape[1]]] for i in range(num_grids)]
+    # visited_and_distance[source[0]*grids.shape[1]+source[1]] = [0,0,cost_map[int(i/grids.shape[1]),i%grids.shape[1]]]
+
+    # for vertex in range(num_grids):
+    #     to_visit = to_be_visited(num_grids,visited_and_distance)
+    #     for neighbor in range(num_grids):
+    #         if vertices[to_visit][neighbor] == 1 and visited_and_distance[neighbor][0] == 0:
+    #             new_cost = visited_and_distance[to_visit][1] + edges[to_visit][neighbor] + visited_and_distance[to_visit][2]
+    #             if visited_and_distance[neighbor][1] > new_cost:
+    #                 visited_and_distance[neighbor][1] = new_cost
+    #         visited_and_distance[to_visit][0] = 1
+    # print(visited_and_distance)
+    return path
+
 def probabilities_(grids,arena_w,arena_h):
     prob = np.zeros_like(grids)
     for i in range(grids.shape[0]):
@@ -128,19 +287,22 @@ def update_prob_costs(grids,present_locs,arena_h,arena_w):
             for l in present_locs:
                 if np.linalg.norm(loc-np.array(l))==0:
                     grids[loc[0],loc[1]].probability -= 1/(arena_h*arena_w)
+                    if grids[loc[0],loc[1]].probability<0:
+                        grids[loc[0],loc[1]].probability=0
                     grids[loc[0],loc[1]].cost += 1
                 else:
                     grids[loc[0],loc[1]].probability += 1*len(present_locs)/(arena_h*arena_w*(arena_h*arena_w))
 
-def plot_mesh(fig,ax,arena_w,arena_h,map):
+def plot_mesh(fig,ax,arena_w,arena_h,map,without_bar=0):
     cs = ax.pcolormesh(range(arena_w),range(arena_h),map,shading='auto')
     # neg = ax.imshow(map, cmap='Reds_r', interpolation='none')
-    cbar = fig.colorbar(cs,orientation='vertical')
-    cbar.set_ticks(np.arange(0,1,0.1))
+    if not without_bar:
+        cbar = fig.colorbar(cs,orientation='vertical')
+        cbar.set_ticks(np.arange(0,1,0.1))
     cs.set_clim(0,1)
     ax.set_aspect('equal', 'box')
-    major_ticks = np.arange(0, 101, 1)
-    minor_ticks = np.arange(0, 101, 1)
+    major_ticks = np.arange(0, arena_w+1, 1)
+    minor_ticks = np.arange(0, arena_h+1, 1)
     ax.set_xticks(major_ticks)
     ax.set_xticks(minor_ticks, minor=True)
     ax.set_yticks(major_ticks)
@@ -150,10 +312,10 @@ def plot_mesh(fig,ax,arena_w,arena_h,map):
     ax.axes.xaxis.set_ticks([])
     ax.axes.yaxis.set_ticks([])
 
-arena_w = 100
-arena_h = 100
+arena_w = 10
+arena_h = 10
 
-grids = np.array([[Node(i,arena_h=arena_h,arena_w=arena_w) for i in range(arena_w)] for j in range(arena_h)])
+grids = np.array([Node(i,arena_h=arena_h,arena_w=arena_w) for i in range(arena_w*arena_h)]).reshape((arena_h,arena_w))
 probabilities = probabilities_(grids,arena_w,arena_h)
 
 plt.ion()
@@ -170,7 +332,8 @@ for num_robots in range(5,25,5):
     
     robots = searchers(num_robots,arena_w,arena_h,grids,ax)
     intruder = intruder_(arena_w,arena_h,grids,ax)
-
+    ax.clear()
+    plot_mesh(fig,ax,arena_w,arena_h,probabilities,without_bar = 1)
     while not search_state: #   While Intruder is not found
         # ax.clear()
         present_locs = visited_loc(robots)
@@ -204,12 +367,16 @@ for num_robots in range(5,25,5):
         for i in range(num_robots): #   Traverse each robot to new location from its past location and check presence of Intruder along the path
             robots[i].next_loc = robots[i].loc(grids)
 
-            path = path_finder(robots[i].present_loc,robots[i].next_loc,grids,fig,ax)
-            
-            for p in path:
-                ax.scatter([path[0][1]+0.5],[path[0][0]+0.5],edgecolors='green', facecolors='none')
-                ax.scatter([p[1]+0.5],[p[0]+0.5],color='yellow',s=5)
-                ax.scatter([path[-1][1]+0.5],[path[-1][0]+0.5],edgecolors='white', facecolors='none')
+            for g in grids:
+                for g_ij in g:
+                    g_ij.parent = None
+            path = Astar(grids,robots[i].present_loc,robots[i].next_loc)#path_finder(robots[i].present_loc,robots[i].next_loc,grids,fig,ax)
+
+            for p in range(len(path)-1):
+                ax.scatter([path[p][1]+0.5],[path[p][0]+0.5],color='yellow',s=5)
+                ax.plot([path[p][1]+0.5,path[p+1][1]+0.5],[path[p][0]+0.5,path[p+1][0]+0.5],color= 'black',alpha=0.2)
+            ax.scatter([path[0][1]+0.5],[path[0][0]+0.5],edgecolors='white', facecolors='none')
+            ax.scatter([path[-1][1]+0.5],[path[-1][0]+0.5],marker='x',color = 'black')
             plt.show()
             plt.pause(0.001)
             
@@ -231,8 +398,9 @@ for num_robots in range(5,25,5):
     time.append(t)  #   Store time taken to find the intruder
     agents.append(num_robots)   #   Store number of robots utilized for search of static intruder
 
-
-plt.plot(t,agents)
-plt.xlabel("Time taken")
-plt.ylabel("Number of search agents")
+plt.ioff()
+fig,ax = plt.subplots()
+ax.plot(time,agents)
+ax.set_xlabel("Time taken")
+ax.set_ylabel("Number of search agents")
 plt.show()
